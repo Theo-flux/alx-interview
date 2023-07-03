49f9073b7d0e7cc8094982ee7716a51277eed841
Theo <ekunnusitheophilus@gmail.com>
2023-07-03 01:28:00 +0100

    Pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
-from math import factorial
+"""
+0-pascal's triangle
+"""
 
 
 def pascal_triangle(num):
+    """
+    that returns a list of lists of
+    integers representing the Pascal’s
+    triangle of n
+
+    Args:
+        num (int): the length of the list
+
+    Returns:
+        list: a list of list of integers
+    """
     a = []
 
     if num < 0:
         return a
 
-    for n in range(num):
+    for n in range(1, num+1):
+        c = 1
         e = []
-        for r in range(n+1):
-            e.append(factorial(n)//(factorial(n-r)*factorial(r)))
+        for r in range(1, n+1):
+            e.append(c)
+            c = c*(n-r)//r
+
         a.append(e)
     return a
