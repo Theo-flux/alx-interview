7ee61557b6eddcc13d6d5e34ab6733220e520f68
Theo <ekunnusitheophilus@gmail.com>
2023-07-07 12:45:56 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,34 +5,25 @@ python file for lockboxes function
 from typing import List, Union
 
 
-def canUnlockAll(boxes: List[List[Union[int, None]]]) -> bool:
+def canUnlockAll(boxes: List[Union[List[int], List]]) -> bool:
     """
     method to determine if all boxes can be opened
 
     Args:
-        boxes (List[List[Union[int, None]]]): list if list of box param
+        boxes (List[Union[List[int], List[]]]): list if list of box param
 
     Returns:
         bool: return type
     """
-    unlocked_boxes = [
-        True if i == 0
-        else False
-        for i in range(len(boxes))
-    ]
-
-    # print("--before--")
-    # print(unlocked_boxes)
-
-    for i in range(len(boxes) - 1):
-        if (len(boxes[i]) == 0):
-            continue
-        else:
-            for j in range(len(boxes[i])):
-                if (boxes[i][j] <= len(boxes) - 1):
-                    unlocked_boxes[boxes[i][j]] = True
-
-    # print("--after--")
-    # print(unlocked_boxes, end='\n\n')
-
-    return all(unlocked_boxes)
+    if len(boxes) == 0:
+        return False
+    if len(boxes) == 1:
+        return True
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    if len(keys) == len(boxes):
+        return True
+    return False
