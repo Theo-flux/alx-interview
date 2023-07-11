722f734323dd053506e4ab15e9c67f18cefa6502
Theo <ekunnusitheophilus@gmail.com>
2023-07-11 19:08:46 +0100

    fix: min operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,59 +4,25 @@ min_operations file
 """
 
 
-def minOperations(n):
-    '''calculates the fewest number of
-    operations needed to result in exactly n H
-    characters in this file.
-    Returns:
-        Integer : if n is impossible to achieve, return 0
-    '''
-    pasted_chars = 1  # how many chars in the file
-    clipboard = 0  # how many H's copied
-    counter = 0  # operations counter
+def minOperations(n) -> int:
+    min_op = 0
 
-    while pasted_chars < n:
-        # if did not copy anything yet
-        if clipboard == 0:
-            # copyall
-            clipboard = pasted_chars
-            # increment operations counter
-            counter += 1
+    if n < 2:
+        return min_op
 
-        # if haven't pasted anything yet
-        if pasted_chars == 1:
-            # paste
-            pasted_chars += clipboard
-            # increment operations counter
-            counter += 1
-            # continue to next loop
-            continue
+    file_content = 'H'
 
-        remaining = n - pasted_chars  # remaining chars to Paste
-        # check if impossible by checking if clipboard
-        # has more than needed to reach the number desired
-        # which also means num of chars in file is equal
-        # or more than in the clipboard.
-        # in both situations it's impossible to achieve n of chars
-        if remaining < clipboard:
-            return 0
 
-        # if can't be devided
-        if remaining % pasted_chars != 0:
-            # paste current clipboard
-            pasted_chars += clipboard
-            # increment operations counter
-            counter += 1
+    while len(file_content) < n:
+        copyAll = file_content
+        min_op += 1
+
+        if len(copyAll + file_content) < n:
+            file_content += copyAll
+            min_op += 1
+            file_content += copyAll
+            min_op += 1
         else:
-            # copyall
-            clipboard = pasted_chars
-            # paste
-            pasted_chars += clipboard
-            # increment operations counter
-            counter += 2
+            break
 
-    # if got the desired result
-    if pasted_chars == n:
-        return counter
-    else:
-        return 0
+    return min_op
