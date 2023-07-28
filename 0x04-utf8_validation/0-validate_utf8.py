ad9e9d465ec7c930744ec570de206ed5cf8c4eb0
Theo <ekunnusitheophilus@gmail.com>
2023-07-28 04:01:27 +0100

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,12 +15,20 @@ def validUTF8(data: List[int]) -> bool:
     Returns:
         bool: _description_
     """
+    byte_count = 0
 
-    flag = True
-
-    for i in range(len(data)):
-        if (data[i] > 127):
-            flag = False
-            break
-
-    return flag
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
