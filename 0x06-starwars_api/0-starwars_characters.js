168d30b994368de4148cc7410b4b005caff8f004
Theophilus <ekunnusitheophilus@gmail.com>
2023-08-09 04:43:45 -0700

    ongoing...

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,2 +1,37 @@
 #!/usr/bin/node
-console.log(process.argv[2])
+const request = require('request')
+
+const peopleList = []
+let charactersUriList = []
+const id = process.argv[2]
+const filmUrl = `https://swapi-api.alx-tools.com/api/films/${id}`
+
+request({uri: filmUrl, json: true}, (error, res) => {
+		if (error) {
+			console.log(error)
+		} else if (res.error) {
+			 console.log(res.error)
+		} else {
+			console.log(res.body['characters'])
+			charactersUriList = res.body['characters']
+		}
+	})
+
+async function getCharacter(uri){
+	await request({uri: uri, json: true}, (error, res) => {
+		if(error) {
+			console.log(error)
+		} else if (res.error) {
+			console.log(res.error)
+		} else {
+			peopleList.push(res.body['name'])
+		}
+	})
+}
+
+charactersUriList.map(async (uri, _) => {
+	await getCharacter(uri)
+	return
+})
+
+console.log("await!",peopleList)
