298e6fbbb6dcd9242ce35c4c16d98465dc6ff901
Theo <ekunnusitheophilus@gmail.com>
2023-08-17 21:49:36 +0100

    fix: rotate 2d

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -16,8 +16,7 @@ def rotate_2d_matrix(matrix: List[List]) -> None:
 
     for i in range(n):
         for j in range(i, n):
-            matrix[i][j] = matrix[j][i]
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    for i in range(n//2):
-        for j in range(n):
-            matrix[j][i], matrix[j][n-1-i] = matrix[j][n-1-i], matrix[j][i]
+    for i in range(n):
+        matrix[i].reverse()
